bc4d3d201caea245e25998332a1cfcacc9bc8a3c
kanghaizhen <kanghaizhen@flashhold.com>
2025-06-12 10:34:37 +0800

    天津数据类型文章

--- src/.vitepress/config/sidebar/js.ts
+++ src/.vitepress/config/sidebar/js.ts
@@ -5,6 +5,7 @@ export const js = {
       items: [
         { text: '概览', link: '/js/' },
         { text: '变量', link: '/js/变量' },
+        { text: '数据类型详解', link: '/js/数据类型' },
       ]
     }
   ],
