ebb3cff8b60b3133e2c797cbedda524e4422edd7
kanghaizhen <kanghaizhen@quicktron.com>
2025-09-04 10:58:13 +0800

    添加文章

--- src/.vitepress/config/sidebar/js.ts
+++ src/.vitepress/config/sidebar/js.ts
@@ -6,6 +6,10 @@ export const js = {
         { text: '概览', link: '/js/' },
         { text: '变量', link: '/js/变量' },
         { text: '数据类型详解', link: '/js/数据类型' },
+        { text: '运算符详解', link: '/js/运算' },
+        { text: '位运算详解', link: '/js/位运算详解' },
+        { text: '条件控制', link: '/js/条件控制' },
+        { text: '循环的使用', link: '/js/循环' },
       ]
     }
   ],
